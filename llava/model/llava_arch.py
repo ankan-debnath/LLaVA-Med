76c3e4dff619358816bddac325949828852cbe2c
Ankan Debnath <ankan_debnath@epam.com>
2025-05-24 00:48:59 +0530

    removing debug statemtn from prepare_inputs_for_multimodel function

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -124,7 +124,6 @@ class LlavaMetaForCausalLM(ABC):
         self, input_ids, position_ids, attention_mask, past_key_values, labels, images, image_sizes=None
     ):
         vision_tower = self.get_vision_tower()
-        print(vision_tower, images, input_ids)
         if vision_tower is None or images is None or input_ids.shape[1] == 1:
             if past_key_values is not None and vision_tower is not None and images is not None and input_ids.shape[1] == 1:
                 target_shape = past_key_values[-1][-1].shape[-2] + 1
